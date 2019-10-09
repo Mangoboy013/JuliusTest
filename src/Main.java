d81bd746940840044b25520985ba2bbdd6269898
Julius <juliuskreutz.jk@gmail.com>
2019-10-10 01:56:25 +0200

    Ja

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // sout -> System.out.println(); wirst diesen shortcut öfters mal benutzen
+
+        //https://www.oracle.com/technetwork/java/codeconventions-135099.html lies dir die naming conventions durch, bekommst punktabzüge für sowas
     }
 }
