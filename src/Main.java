352ca9d510d14191c284e13ea7ac74504c83cc28
Julius <juliuskreutz.jk@gmail.com>
2019-10-10 01:57:51 +0200

    Ja

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
+/*<-- Auf diesen grünen Pfeil drücken um die main methode auszuführen*/ public static void main(String[] args) {
 	    // sout -> System.out.println(); wirst diesen shortcut öfters mal benutzen
+
+        System.out.println("Hello World");
 
         //https://www.oracle.com/technetwork/java/codeconventions-135099.html lies dir die naming conventions durch, bekommst punktabzüge für sowas
     }
