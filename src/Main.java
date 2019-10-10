1f3dd8b02575fd2e67e1f17c87d2243df9a6c531
Julius <juliuskreutz.jk@gmail.com>
2019-10-10 22:09:00 +0200

    Ja

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 /*<-- Auf diesen grünen Pfeil drücken um die main methode auszuführen*/ public static void main(String[] args) {
 	    // sout -> System.out.println(); wirst diesen shortcut öfters mal benutzen
 
-        System.out.println("Hello World, test, kein test");
+        System.out.println("Hello World");
 
         //https://www.oracle.com/technetwork/java/codeconventions-135099.html lies dir die naming conventions durch, bekommst punktabzüge für sowas
     }
