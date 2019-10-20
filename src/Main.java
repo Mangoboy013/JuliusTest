6d2a7e8b004df599a35fb4d424f9c0290411c4e5
Julius <juliuskreutz.jk@gmail.com>
2019-10-20 17:52:09 +0200

    Habe ads un das geändert

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello World");
 
+
         //https://www.oracle.com/technetwork/java/codeconventions-135099.html lies dir die naming conventions durch, bekommst punktabzüge für sowas
     }
 }
